82ce81d5e848c030a2921bfe71b68892f584d723
WanderGolem <mathias500r@gmail.com>
2026-04-09 05:12:24 +0000

    C: Token Metadata

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from google import genai
 def main():
 
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
     if api_key == None:
         raise RuntimeError("no API-Key is set")
 
     client = genai.Client(api_key=api_key)
-
+    user_promt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.5-flash", 
+        contents=user_promt
     )
 
+    if response.usage_metadata == None:
+        raise RuntimeError("API request failed")
+    
+    print(f"User Promt: {user_promt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
