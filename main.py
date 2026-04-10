3f3cf4d29f8b442fb66414a802abd1aee1e3f565
WanderGolem <mathias500r@gmail.com>
2026-04-10 04:50:10 +0000

    E: Multiple Messages

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
     
-    if api_key == None:
+    if not api_key:
         raise RuntimeError("no API-Key is set")
 
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
-        contents=args.user_prompt
+        contents=messages
     )
 
-    if response.usage_metadata == None:
+    if not response.usage_metadata:
         raise RuntimeError("API request failed")
     
     print(f"User Promt: {args.user_prompt}")
