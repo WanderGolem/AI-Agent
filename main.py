5d87b63c7da522d7a1e003482235da3127fd7484
WanderGolem <mathias500r@gmail.com>
2026-04-11 11:58:43 +0000

    F: Verbose Output

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.genai import types
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     
@@ -28,9 +29,11 @@ def main():
     if not response.usage_metadata:
         raise RuntimeError("API request failed")
     
-    print(f"User Promt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response:")
     print(response.text)
 
