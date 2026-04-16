5c7e01b2005ac5d460aeb8587475d43279594103
WanderGolem <mathias500r@gmail.com>
2026-04-16 19:26:51 +0000

    N: Function Declaration

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 def main():
+    load_dotenv()
+
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -13,31 +16,38 @@ def main():
     
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    load_dotenv()
-
     api_key = os.environ.get("GEMINI_API_KEY")
     
     if not api_key:
         raise RuntimeError("no API-Key is set")
 
     client = genai.Client(api_key=api_key)
+    
+    generate_content(client, messages, args.verbose, args.user_prompt)    
+
+
+def generate_content(client, messages, verbose, user_prompt): 
 
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
     )
 
     if not response.usage_metadata:
         raise RuntimeError("API request failed")
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print("Response:")
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+           print(f"Calling function: {function_call.name}({function_call.args})" )
+    else:
+        print("Response:")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
