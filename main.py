87a0d8b89f6beab4e4c1af82bc288422bdc10c21
WanderGolem <mathias500r@gmail.com>
2026-04-18 18:50:24 +0000

    P: Calling Functions

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
+from google.genai import types, errors
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     load_dotenv()
@@ -27,27 +27,42 @@ def main():
 
 
 def generate_content(client, messages, verbose, user_prompt): 
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
+        )
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
-    )
+        if not response.usage_metadata:
+            raise RuntimeError("API request failed")
 
-    if not response.usage_metadata:
-        raise RuntimeError("API request failed")
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.function_calls:
+            function_results = []
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose)
 
-    if response.function_calls:
-        for function_call in response.function_calls:
-           print(f"Calling function: {function_call.name}({function_call.args})" )
-    else:
-        print("Response:")
-        print(response.text)
+                if not function_call_result.parts:
+                    raise Exception("empty parts in function call result")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("first item is not a object in function call result")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("empty function call result")
+                
+                function_results.append(function_call_result.parts[0])
 
+                if verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print("Response:")
+            print(response.text)
+            
+    except errors.APIError as e:
+        print(f"API error: {e}")
 if __name__ == "__main__":
     main()
